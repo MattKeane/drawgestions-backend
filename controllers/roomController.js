b1e4e218b38fc519325a700b406fa2e9ccd31a83
Matt Keane <mattpkeane@gmail.com>
2021-11-12 15:50:52 -0600

    new room route now creates a room in db

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -1,17 +1,38 @@
 const express = require('express')
 
+const Room = require('../models/room')
+
 const router = express.Router()
 
-router.get('/new', (req, res) => {
-    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    let desiredAccessCode = ''
-    while (desiredAccessCode.length < 4) {
-        const randomChoice = Math.floor(Math.random() * 26)
-        desiredAccessCode += alphabet[randomChoice]
+router.get('/new', async (req, res) => {
+    try {
+        const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        let desiredAccessCode = ''
+        while (desiredAccessCode.length < 4) {
+            const randomChoice = Math.floor(Math.random() * 26)
+            desiredAccessCode += alphabet[randomChoice]
+            if (desiredAccessCode.length === 4) {
+                const checkCode = await Room.findOne({ accessCode: desiredAccessCode})
+                if (checkCode) {
+                    desiredAccessCode = ''
+                } else {
+                    const createdRoom = await Room.create({
+                        accessCode: desiredAccessCode,
+                    })
+                    res.json({
+                        accessCode: desiredAccessCode
+                    })
+                }
+            }
+        }
+    } catch (err) {
+        const d = new Date()
+        console.log(`${d.toLocaleString()}: Error creating a room`)
+        console.log(err)
+        res.json({
+            message: 'Error creating room'
+        })
     }
-    res.json({
-        accessCode: desiredAccessCode
-    })
 })
 
 module.exports = router
