1e273df794a8de7d49bafb3b6f62cf9d30fa5243
Matt Keane <mattpkeane@gmail.com>
2021-11-24 15:52:01 -0600

    now emits a random suggestion once all suggestions have been submitted

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,7 +16,6 @@ router.post('/new', async (req, res) => {
                     })
                 } else {
                     const newUser = await User.create(req.body)
-                    console.log(newUser)
                     roomToJoin.users.push(newUser)
                     await roomToJoin.save()
                     const users = roomToJoin.users.map(user => user.displayName)
--- server.js
+++ server.js
@@ -18,7 +18,6 @@ const io = new Server(server, {
 })
 
 io.on('connection', socket => {
-    console.log('Socket connected: ', socket.id)
 
     socket.on('join', room => {
         socket.join(room)
@@ -46,13 +45,45 @@ io.on('connection', socket => {
 
     socket.on('suggestion', async suggestion => {
         try {
-            console.log(socket.id)
-            const user = await User.findOne({ socket: socket.id })
-            console.log(user)
-            if (user.suggestions.length < 3) {
-                user.suggestions.push(suggestion)
-                await user.save()
-                console.log(user)
+            const suggester = await User.findOne({ socket: socket.id })
+            // only allow the user to add 3 suggestions
+            if (suggester.suggestions.length < 3) {
+                suggester.suggestions.push(suggestion)
+                await suggester.save()
+                if (suggester.suggestions.length === 3) {
+                    const room = await Room
+                        .findOne({ users: suggester })
+                        .populate('users')
+                    // check if all users have submited 3 suggestions
+                    if (room.users.every(user => user.suggestions.length >= 3)) {
+                        room.users.forEach(async user => {
+                            try {
+                                const othersSuggestions = room.users.reduce((acc, cur) => {
+                                    if (cur !== user) {
+                                        cur.suggestions.forEach((suggestion, i) => {
+                                            const suggestionData = {
+                                                suggestion,
+                                                user: cur,
+                                                index: i,
+                                            }
+                                            acc.push(suggestionData)
+                                        })
+                                    }
+                                    return acc
+                                }, [])
+                                const randomIndex = Math.floor(Math.random() * othersSuggestions.length)
+                                const chosen = othersSuggestions[randomIndex]
+                                io.to(user.socket).emit('drawThis', chosen.suggestion)
+                                chosen.user.suggestions.splice(chosen.index, 1)
+                                await chosen.user.save()
+                            } catch (err) {
+                                const d = new Date()
+                                console.log(`${d.toLocaleString()}: Error assigning suggestions`)
+                                console.log(err)
+                            }
+                        })
+                    }
+                }
             }
         } catch (err) {
             const d = new Date()
